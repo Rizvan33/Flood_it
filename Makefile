9f37f60ab0516441ce28977ce8b1fefb65abc130
Rizvan33 <55735154+Rizvan33@users.noreply.github.com>
2020-05-06 16:19:12 +0200

    Pour calculer temps d'exécution

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: Flood-It_Partie1
+all: Flood-It_Partie1 temps
 
 API_Gene_instance.o: API_Gene_instance.c API_Gene_instance.h
 	gcc -c API_Gene_instance.c
@@ -19,7 +19,14 @@ Flood-It_Partie1.o: Flood-It_Partie1.c
 	gcc -c Flood-It_Partie1.c 
 
 Flood-It_Partie1: Flood-It_Partie1.o Liste_case.o  API_Grille.o API_Gene_instance.o Fonctions_exo1.o S_Zsg.o
-	gcc -o bin/Flood-It_Partie1 Flood-It_Partie1.o Liste_case.o API_Grille.o API_Gene_instance.o Fonctions_exo1.o S_Zsg.o  -I include -L lib -lmingw32 -lSDL2main -lSDL2
+	gcc -o bin/Flood-It_Partie1 Flood-It_Partie1.o Liste_case.o API_Grille.o API_Gene_instance.o Fonctions_exo1.o S_Zsg.o  -I include -L lib -lmingw32 -lSDL2main -lSDL2 
+
+temps.o : temps.c
+	gcc -c temps.c
+
+temps : temps.o Liste_case.o  API_Grille.o API_Gene_instance.o Fonctions_exo1.o S_Zsg.o
+	gcc -o bin/temps temps.o Liste_case.o API_Grille.o API_Gene_instance.o Fonctions_exo1.o S_Zsg.o  -I include -L lib -lmingw32 -lSDL2main -lSDL2
+
 
 clean:
 	rm -f *.o Flood-It_Partie1
---
+++ temps.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "API_Grille.h"
+#include "API_Gene_instance.h"
+#include "Entete_Fonctions.h"
+#include "Liste_case.h"
+#include "S_Zsg.h"
+void simulation_jeu(int dim, int nbcl, int nivdif, int graine, int aff, int mode,float *temps_cpu ){
+	clock_t
+		temps_initial, // Temps initial en micro-secondes
+		temps_final;   // Temps final en micro-secondes
+		
+	Grille *G;
+	
+	int **M;
+	/* Generation de l'instance */
+	
+	M=(int **) malloc(sizeof(int*)*dim);
+	int i,j;
+	for (i=0;i<dim;i++){
+		M[i]=(int*) malloc(sizeof(int)*dim);
+		if (M[i]==0) 
+			printf("Pas assez d'espace mémoire disponible\n");
+	}
+
+	Gene_instance_genere_matrice(dim, nbcl, nivdif, graine, M);
+
+	/* Affichage de la grille */
+
+	Grille_init(dim,nbcl,500,&G);
+
+	Grille_ouvre_fenetre(G);
+
+	for (i=0;i<dim;i++)
+		for (j=0;j<dim;j++)
+ 		Grille_attribue_couleur_case(G,i,j,M[i][j]);
+	
+	Grille_redessine_Grille(G);
+	
+	if(mode==1){
+		temps_initial=clock();
+		sequence_aleatoire_rec(M, G,dim,G->nbcl,1);
+		temps_final=clock();
+	}
+	else if(mode==2){
+		temps_initial=clock();
+		sequence_aleatoire_rec_imp(M, G,dim,G->nbcl,1);
+		temps_final=clock();
+	}
+	else if(mode==3){
+		temps_initial=clock();
+		S_Zsg * Z=init_Zsg(dim,nbcl);
+		strequence_aleatoire_rapide(M,G,Z);
+		temps_final=clock();
+	}
+			
+	*temps_cpu=temps_final-temps_initial;
+
+	//Grille_attente_touche();
+
+	Grille_ferme_fenetre();
+
+	Grille_free(&G);
+
+}
+
+void test_dimension(int n){
+
+	FILE *f1=fopen("dimrec.txt","w");
+	FILE *f2=fopen("dimimp.txt","w");
+	FILE *f3=fopen("dimzsg.txt","w");
+	
+	int i;
+	
+	float *temps_cpu=(float *)malloc(sizeof(float));
+	printf("Test de la dimension\n");
+	for(i=1;i<=n;i++){
+		printf("i=%d\n",i);
+		simulation_jeu(i,5,1,1,0,1,temps_cpu);
+		fprintf(f1,"%d %f\n",i,*temps_cpu);
+		simulation_jeu(i,5,1,1,0,2,temps_cpu);
+		fprintf(f2,"%d %f\n",i,*temps_cpu);
+		simulation_jeu(i,5,1,1,0,3,temps_cpu);
+		fprintf(f3,"%d %f\n",i,*temps_cpu);
+	}
+	printf("\n");
+	fclose(f1); 
+	fclose(f2); 
+	fclose(f3);	
+}
+
+
+int main(int argc,char**argv){
+	int dim;
+	printf("Entrez une dimension maximale:");
+	scanf("%d",&dim);
+	test_dimension(dim);
+	return 0;
+}
+
